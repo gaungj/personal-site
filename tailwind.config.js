a0d003598054c5368f135d3aa99fb31cbc56ba68
gaungj <gaung.jgd@gmail.com>
2024-06-24 19:50:45 +0700

    feat: initial page header, home, and about sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#0891b2",
+        secondary: "#e0f2fe",
+        tersier: "#64748b",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
   },
   plugins: [],
-}
-
+};
